647ca0c2e43a32ca6fd595873f5a725ab47aa1b6
Michael Jordan <mjordan9@gmail.com>
2015-02-02 11:21:24 -0500

    updated seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ Player.each_with_index do |p, i|
   Idea.create(
     revenue: 0,
     costs: 0, 
-    description: 'Super awesoem amazing idea here'
+    description: 'Super awesoem amazing idea here',
     player_id: p.id
     )
 end
