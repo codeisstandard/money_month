dba591fc369d767f7495ce4938fe49d915ec2375
Jared Malan <jfals82@gmail.com>
2015-02-12 10:06:31 -0500

    removed dip

--- db/migrate/20150203152042_remove_money_from_ideas.rb
+++
@@ -1,6 +0,0 @@
-class RemoveMoneyFromIdeas < ActiveRecord::Migration
-  def change
-    remove_column :ideas, :revenue, :float
-    remove_column :ideas, :costs, :float
-  end
-end
--- db/migrate/20150203210751_remove_dip_from_ideas.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDipFromIdeas < ActiveRecord::Migration
-  def change
-    remove_column :ideas, :dip, :string
-  end
-end
